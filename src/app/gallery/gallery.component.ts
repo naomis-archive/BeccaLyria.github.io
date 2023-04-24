e543e36e04097c5f258edfa386be4c2eb774d19a
a2937 <a.rcottrill521@gmail.com>
2023-04-24 15:30:40 -0400

    feat: create an image view box (#666)
    * Start a view box.
    
    * Include a header with the full-screen image
    
    * Add documentation for my functions.
    
    * Add some more basic comments.
    
    * Use responsive units
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    * Center the image
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>
    
    ---------
    
    Co-authored-by: Naomi Carrigan <nhcarrigan@gmail.com>

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -6,11 +6,35 @@ import { ArtInt, artList } from 'src/assets/data/artList';
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css'],
 })
+
+  /**
+   * The page that displays all of the accumulated BeccaLyria artwork.
+   */
 export class GalleryComponent implements OnInit {
   artList: ArtInt[] = [];
-  constructor() {}
 
+  selectedArt?: ArtInt;
+
+  /**
+   * Loads all the images to be displayed in the gallery when the page is loaded.
+   */
   ngOnInit(): void {
     this.artList = artList;
   }
+
+  /**
+   * Sets the artwork to be seen in the full screen view
+   * @param art The art intended for full-screen view
+   */
+  viewCloser(art: ArtInt) {
+    this.selectedArt = art;
+  }
+
+  /**
+   * Closes the full screen view of the image.
+   */
+  closeModal() {
+    this.selectedArt = undefined;
+  }
 }
+
